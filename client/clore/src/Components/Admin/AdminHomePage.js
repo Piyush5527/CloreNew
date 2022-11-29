137f406e9d5ad1e7d4ba4ee4065c51139c7fc73b
Piyush5527 <piyushjainkgp36@gmail.com>
2022-11-29 15:34:28 +0530

    admin dashboard design

--- client/clore/src/Components/Admin/AdminHomePage.js
+++ client/clore/src/Components/Admin/AdminHomePage.js
@@ -1,11 +1,26 @@
-import React,{Fragment} from 'react'
-import AdminNavbar from './AdminNavbar'
-
+import React, { Fragment } from 'react'
+import AdminDashboardCard from './AdminDashboardCard';
+import AdminNavbar from './AdminNavbar';
+import styles from "../../CSS/Admin/AdminHomePage.module.css";
 const AdminHomePage = () => {
   return (
     <Fragment>
-        <AdminNavbar/>
-        AdminHomePage
+      <AdminNavbar />
+      <div className={styles.main_container}>
+        <AdminDashboardCard header='User Count' value='testing' />
+        <AdminDashboardCard header='Product Count' value='testing' />
+        <AdminDashboardCard header='Order Count' value='testing' />
+        <AdminDashboardCard header='Orders Completed' value='testing' />
+        <AdminDashboardCard header='Total Sales' value='testing' />
+        <AdminDashboardCard header='This Month Sales' value='testing' />
+        <AdminDashboardCard header='Unit Selled' value='testing' />
+        <AdminDashboardCard header='Low Stock Quantity' value='testing' />
+        <div className={styles.container_center}>
+          <AdminDashboardCard header='Brands Count' value='testing' />
+          <AdminDashboardCard header='Category Count' value='testing' />
+          <AdminDashboardCard header='Sub Category Count' value='testing' />
+        </div>
+      </div>
     </Fragment>
   )
 }
