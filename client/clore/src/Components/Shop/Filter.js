cbb4eff865212d41233a9aca675cc01a8451863b
piyush5527 <piyushjainkgp36@gmail.com>
2022-12-06 15:38:42 +0530

    filter working but not perfectly

--- client/clore/src/Components/Shop/Filter.js
+++ client/clore/src/Components/Shop/Filter.js
@@ -1,10 +1,13 @@
 import React, { Fragment, useState, useEffect } from 'react'
 import styles from '../../CSS/ShopPage/Filter.module.css';
-const Filter = () => {
+const Filter = (props) => {
 
     const [brand, setBrand] = useState([]);
     const [category, setCategory] = useState([]);
     const [size, setSize] = useState({});
+    const [brandFilter,setBrandFilter]=useState()
+    const [categoryFilter,setCategoryFilter]=useState();
+    const [sizeFilter,setSizeFilter]=useState();
     let sizeList = []
 
     const getbrand = async () => {
@@ -58,16 +61,39 @@ const Filter = () => {
         getCateory();
         getSize();
     }, [])
+    const submitHandler = async (e) => {
+
+        e.preventDefault();
+        props.onFilterDataSubmitHandler(brandFilter,categoryFilter,sizeFilter)
+    }
+
+    // useEffect(()=>{
+    //     console.log("category",categoryFilter,"brand",brandFilter,"size",sizeFilter);
+    //     props.onFilterDataSubmitHandler(brandFilter,categoryFilter,sizeFilter)
+    // },[brandFilter,categoryFilter,sizeFilter])
+    useEffect(()=>{
+        props.onBrandChange(brandFilter)   
+    },[brandFilter])
+    useEffect(()=>{
+        props.onCategoryChange(categoryFilter)
+    },[categoryFilter]);
+    useEffect(()=>{
+        props.onSizeChange(sizeFilter)
+    },[sizeFilter])
     return (
         <Fragment>
             <div className={styles.main_container}>
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className={styles.filter_div_container}>
                         <h5>Brands</h5>
 
                         {
                             brand.map((item) => {
-                                return (<div>{item.brand_name}<input type="checkbox" value={item.brand_name} /></div>)
+                                return (<div>{item.brand_name}<input type="radio" name="brands" value={item._id} 
+                                    onChange={(e)=>{
+                                        setBrandFilter(e.target.value);
+                                    }}
+                                /></div>)
                             })
                         }
 
@@ -77,7 +103,11 @@ const Filter = () => {
                         <h5>Category</h5>
                         {
                             category.map((item) => {
-                                return (<div>{item.category_name}<input type="checkbox" value={item.category_name} /></div>)
+                                return (<div>{item.category_name}<input type="radio" name='categories' value={item._id} 
+                                    onChange={(e)=>{
+                                        setCategoryFilter(e.target.value);
+                                    }}
+                                /></div>)
                             })
                         }
 
@@ -96,11 +126,22 @@ const Filter = () => {
                         {
                             sizeList.length > 0 ?
                                 sizeList.map((item) => {
-                                    return (<div>{item}<input type="checkbox" value={item} /></div>);
+                                    return (<div>{item}<input type="radio" name="lengths" value={item} 
+                                        onChange={
+                                            (e) => {
+                                                setSizeFilter(e.target.value);
+                                            }
+                                        }
+                                    /></div>);
                                 }):""
                         }
                     </div>
-                    <input type="submit" value="search" className='btn btn-secondary' />
+                    <input type="reset" value="Reset" className='btn btn-secondary' onClick={()=>{
+                        setBrandFilter("")
+                        setCategoryFilter("")
+                        setSizeFilter("")
+                        props.onResetFilter();
+                    }}/>
                 </form>
             </div>
 
